8a4cd1dfdfd7f4c8131b84112e8dc0c89bcac764
chandan <chandan7979ck@gmail.com>
2024-02-15 18:47:41 +0530

    feat: homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { AllRoutes } from "./routes/AllRoutes"; 
-import './App.css';
+import { Header, Footer } from "./components";
 
 function App() {
   return (
 
     <div className="App">
-      <AllRoutes />
+      < Header />
+      < AllRoutes />
+      < Footer />
     </div>
   
   );
---
+++ src/pages/Cart/CartPage.js
@@ -0,0 +1,8 @@
+
+export const CartPage = () => {
+  return (
+    <main>
+      
+    </main>
+  )
+}
